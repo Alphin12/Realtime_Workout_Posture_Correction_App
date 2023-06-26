64592f8fbdf493de54c03b15399049ce672f0ece
Alphin K Sajan <alphinksajan@gmail.com>
2023-06-26 20:51:40 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:workout_posture_correction/video_input.dart';
+import 'package:workout_posture_correction_app/startslidez.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false, //to avoid turning on a debug banner while in debug mode
+        theme: ThemeData.dark().copyWith(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromARGB(255, 35, 197, 230),
+          seedColor:const Color.fromARGB(255, 2, 255, 247),
           brightness: Brightness.dark,
-          surface: Color.fromARGB(255, 38, 41, 42),
+          surface:const Color.fromARGB(255, 41, 45, 44),
         ),
-        scaffoldBackgroundColor: Color.fromARGB(255, 6, 7, 7),
+        scaffoldBackgroundColor:const Color.fromARGB(255, 6, 7, 7),
       ),
-      home: const VideoInput(),
-    );
-  }
+        home: OnBoardingPage(),
+      );
 }
